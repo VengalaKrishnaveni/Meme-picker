dc641bf7f4db6e47b53d2fa5e06198268571dadf
Vengala Krishnaveni <97837851+VengalaKrishnaveni@users.noreply.github.com>
2023-08-11 16:18:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { catsData } from "./data.js"
 
+//get the list of emotions of available images
 function getEmotionsArray(cats){
     const emotionsArray = []
     for(let cat of cats){
@@ -15,6 +16,7 @@ function getEmotionsArray(cats){
 const emotionList = document.getElementById('emotion-list')
 
 
+//render the emotions list to the html
 function renderEmotionsList(cats){
     const emotions = getEmotionsArray(cats)
     for(let emotion of emotions){
@@ -25,9 +27,53 @@ function renderEmotionsList(cats){
         </div>`
     }
 }
-
 renderEmotionsList(catsData)
 
-emotionList.addEventListener('click', function(e){
-    console.log(e.target.id)
-})
+emotionList.addEventListener('change', highlightSelectedOption)
+
+function unhighlightSelectedOption(e){
+    const radios = document.getElementsByClassName('radio')
+    for(let radio of radios){
+        radio.classList.remove('highlight')
+    }
+}
+
+function highlightSelectedOption(e){
+    unhighlightSelectedOption()
+    const radioSelected = document.getElementById(e.target.id).parentElement
+    radioSelected.classList.add('highlight')
+}
+
+
+
+
+//listen for click event of get image button
+const getImageBtn = document.getElementById('get-image-btn')
+
+getImageBtn.addEventListener('click',getMatchingCatsArray)
+
+function getMatchingCatsArray(){
+    if(document.querySelector('input[type="radio"]:checked').value){
+        const radioSelected = document.querySelector('input[type="radio"]:checked').value
+        console.log(radioSelected)
+        const emotions = catsData.emotionTags
+        const matchingEmotions = emotions.filter(function(emotion){
+            if(emotion === radioSelected){
+                return true
+            }
+            else{
+                return false
+            }
+        })
+        console.log(matchingEmotions)
+    }
+    //gif's only option
+    // if(document.getElementById('gifs-only-option').checked){
+    //     console.log('yes')
+    // }
+    // else{
+    //     console.log('no')
+    // }
+    
+    unhighlightSelectedOption()
+}
